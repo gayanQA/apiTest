093c5254c5fef31f7dc8fe68610527aa52a0a2e7
gayanQA <gayandamsiri@gmail.com>
2022-12-23 12:04:10 +0000

    Change message

--- api_test/get_user.py
+++ api_test/get_user.py
@@ -14,6 +14,6 @@ print('total_pages:',json_response['total_pages'])
 assert json_response['total_pages'] == 2 , "Value doesn't match"
 
 print('Email :', json_response['data'][0]['email'])
-assert json_response['data'][0]['email'].endswith("reqres.in"), "Email format doesn't match"
+assert json_response['data'][0]['email'].endswith("reqres.in"), "Email format should be in proper format"
 
 print('URL :', json_response['support']['url'])
